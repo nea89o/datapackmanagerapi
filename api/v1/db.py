e0d6c5ccdf6576aba56406455d9833de9626f7de
romangraef <roman.graef@gmail.com>
2018-06-09 20:00:38 +0200

    database remodel

--- api/v1/db.py
+++ api/v1/db.py
@@ -1,4 +1,4 @@
-from peewee import SqliteDatabase, ForeignKeyField, CharField, Model, IntegerField
+from peewee import SqliteDatabase, ForeignKeyField, CharField, Model, IntegerField, DateTimeField, BooleanField
 
 db = SqliteDatabase('datapackmanager.db')
 
@@ -11,11 +11,17 @@ class BaseModel(Model):
 class User(BaseModel):
     name = CharField()
     id = IntegerField(primary_key=True)
+    email = CharField()
+    password_hash = CharField()
 
 
 class Category(BaseModel):
     name = CharField()
     id = IntegerField(primary_key=True)
+
+
+class Tag(BaseModel):
+    name = CharField(primary_key=True)
 
 
 class DataPack(BaseModel):
@@ -24,12 +30,32 @@ class DataPack(BaseModel):
     description = CharField(max_length=10000)
     category = ForeignKeyField(Category)
     author = ForeignKeyField(User)
+    likes = IntegerField()
+    dislikes = IntegerField()
+    downloads = IntegerField()
+    views = IntegerField()
+
+
+class TagRelation(BaseModel):
+    tags = ForeignKeyField(Tag)
+    packs = ForeignKeyField(DataPack)
+
+
+class Comment(BaseModel):
+    id = IntegerField(primary_key=True)
+    datapack = ForeignKeyField(DataPack)
+    user = ForeignKeyField(User)
+    mesage = CharField(max_length=2000)
+    creation = DateTimeField()
 
 
 class Version(BaseModel):
+    id = IntegerField(primary_key=True)
     name = CharField()
     datapack = ForeignKeyField(DataPack)
+    prerelease = BooleanField()
+    releaedate = DateTimeField()
 
 
-db.create_tables([DataPack, Category, Version, User])
-__all__ = ('DataPack', 'Category', 'Version', 'db')
+db.create_tables([DataPack, Category, Version, User, Comment, TagRelation, Tag])
+__all__ = ['DataPack', 'Category', 'Version', 'User', 'Comment', 'TagRelation', 'Tag']
